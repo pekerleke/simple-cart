3b0087ee14d1a107d5f3e476b91502edc753980b
Pekerleke <mrtnsalem@gmail.com>
2024-11-18 15:39:45 -0300

    fix invitation metadata

--- src/app/invitation/page.tsx
+++ src/app/invitation/page.tsx
@@ -1,37 +1,29 @@
 import { Suspense } from "react";
 import { InnerInvitationPage } from "./InnerInvitationPage";
+import type { Metadata, ResolvingMetadata } from 'next'
 
-
-export async function generateMetadata({ searchParams }: any) {
+export async function generateMetadata(
+    { params, searchParams }: any,
+    parent: ResolvingMetadata
+): Promise<Metadata> {
     const invitationCode = searchParams.invitationCode;
-  
-    const data = await fetch(
-      `https://simple-cart-ruddy.vercel.app/api/organizations/invite?invitationCode=${invitationCode}`
-    )
-      .then((res) => res.json())
-      .then((response) => response.data)
-      .catch(() => null);
-  
-    const organizationName = data?.organizations?.name || "SimpleCart";
-  
+
+    const invitation = await fetch(`https://simple-cart-ruddy.vercel.app/api/organizations/invite?invitationCode=${invitationCode}`).then((res) => res.json())
+
+    const organizationName = invitation?.data?.organizations?.name || "organization"
+
     return {
-      title: `Join ${organizationName} | Simple Cart`,
-      description: `Accept your invitation to join ${organizationName} on SimpleCart.`,
-      openGraph: {
-        title: `Join ${organizationName}`,
-        description: `Join ${organizationName} on SimpleCart with this invitation.`,
-        url: `https://simple-cart-ruddy.vercel.app/invitation?invitationCode=${invitationCode}`,
-        images: [
-          {
-            url: "https://simple-cart-ruddy.vercel.app/cover.jpg",
-            width: 1200,
-            height: 630,
-          },
-        ],
-        siteName: "Simple Cart",
-      },
-    };
-  }
+        title: `Join ${organizationName} | Simple Cart`,
+        description: `Accept your invitation to join ${organizationName} on SimpleCart.`,
+        openGraph: {
+            title: `Join ${organizationName} | Simple Cart`,
+            description: `Accept your invitation to join ${organizationName} on SimpleCart.`,
+            images: ['https://simple-cart-ruddy.vercel.app/cover.jpg'],
+            url: `https://simple-cart-ruddy.vercel.app/invitation?invitationCode=${invitationCode}`,
+            siteName: "Simple Cart",
+        },
+    }
+}
 
 export default function InvitationPage() {
     return (
